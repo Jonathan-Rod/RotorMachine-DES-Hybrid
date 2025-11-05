188c0374898d9eb14d30eb030f59203348db7da5
Jonathan-Rod <60677254+Jonathan-Rod@users.noreply.github.com>
2025-11-04 20:57:20 -0400

    Implement DES encryption and decryption methods, and add Permutation class skeleton
    Co-authored-by: CovasJJ <CovasJJ@users.noreply.github.com>

--- src/des_encryption.py
+++ src/des_encryption.py
@@ -1,9 +1,43 @@
 class DESEncryption:
-    def __init__(self):
+    def __init__(self, plaintext, key):
+        self.plaintext = plaintext
+        self.key = key
+        self.rounds = 16
+
+    def left_circular_shift(self, key):
+        # TODO
+        return key[1:] + key[0]
+
+    def permuted_choice_1(self, key):
+        # TODO
         pass
 
+    def permuted_choice_2(self, key):
+        # TODO
+        pass
+
+    def round(self, plaintext, permuted_choice_2):
+        # TODO Expansion/permutation with plaintext (E table) (plaintex Right side)
+        # TODO XOR with round key (permuted_choice_2)
+        # TODO S-box substitution
+        # TODO P-box permutation
+        # TODO XOR with plaintext (plaintextLeft side)
+
+        return plaintext
+
     def encrypt(self, text):
+        # Encryption logic
+        key = self.permuted_choice_1(self.key)
+        for i in range(self.rounds):
+            key = self.left_circular_shift(key)
+            permuted_choice_2 = self.permuted_choice_2(key)
+            text = self.round(text, permuted_choice_2)
+
         return text
 
     def decrypt(self, text):
+        # Decryption logic
+        for i in range(self.rounds):
+            pass
+
         return text
---
+++ src/permutation.py
@@ -0,0 +1,12 @@
+class Permutation:
+    def __init__(self, key: str, text: str, key_length: int, text_length: int):
+        self.key = key
+        self.text = text
+        self.key_length = key_length
+        self.text_length = text_length
+
+    def permute(self):
+        pass
+
+    def inverse_permute(self):
+        pass
