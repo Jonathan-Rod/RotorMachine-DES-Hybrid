d552e5e3cd9f2e9cc29f5cdd81083305974dbd4d
Jonathan-Rod <60677254+Jonathan-Rod@users.noreply.github.com>
2025-11-07 12:05:06 -0400

    Refactor DESPermutation class: improve formatting of initial permutation table and implement permuted_choice_1 method for key processing TOFIX

--- src/des_permutation.py
+++ src/des_permutation.py
@@ -8,31 +8,98 @@ class DESPermutation:
 
         # DES Initial Permutation table
         initial_permut_table = [
-            58, 50, 42, 34, 26, 18, 10, 2,
-            60, 52, 44, 36, 28, 20, 12, 4,
-            62, 54, 46, 38, 30, 22, 14, 6,
-            64, 56, 48, 40, 32, 24, 16, 8,
-            57, 49, 41, 33, 25, 17, 9,  1,
-            59, 51, 43, 35, 27, 19, 11, 3,
-            61, 53, 45, 37, 29, 21, 13, 5,
-            63, 55, 47, 39, 31, 23, 15, 7
+            58,
+            50,
+            42,
+            34,
+            26,
+            18,
+            10,
+            2,
+            60,
+            52,
+            44,
+            36,
+            28,
+            20,
+            12,
+            4,
+            62,
+            54,
+            46,
+            38,
+            30,
+            22,
+            14,
+            6,
+            64,
+            56,
+            48,
+            40,
+            32,
+            24,
+            16,
+            8,
+            57,
+            49,
+            41,
+            33,
+            25,
+            17,
+            9,
+            1,
+            59,
+            51,
+            43,
+            35,
+            27,
+            19,
+            11,
+            3,
+            61,
+            53,
+            45,
+            37,
+            29,
+            21,
+            13,
+            5,
+            63,
+            55,
+            47,
+            39,
+            31,
+            23,
+            15,
+            7,
         ]
         block = block_64bits
-    
+
         # Perform permutation
-        permuted = ''
+        permuted = ""
         for pos in initial_permut_table:
             # The permutation table uses 1-based indexing (to match presentation), so subtract 1
-            permuted += block[pos-1]
+            permuted += block[pos - 1]
 
         # Returns 64 bits after initial permutation
-        return permuted  
+        return permuted
 
-
-    def permuted_choice_1(self):
+    def permuted_choice_1(self, key_64bits: str):
         # TODO Permutation of 64-bit key
         # returns 56-bit
-        pass
+        # 8 blocks of 7 bits where each block contains originally a parity bit and is removed
+        # for 1 block remove index 0 bit
+        # for 2 block remove index 1 bit
+        # and so on until index 6
+
+        blocks_7bits = []
+        for i in range(8):
+            block = key_64bits[i * 7 : (i + 1) * 7]
+            block = block[1:]  # Remove parity bit
+            blocks_7bits.append(block)
+
+
+        return blocks_7bits
 
     def permuted_choice_2(self):
         # TODO Permutation of 56-bit key
@@ -44,17 +111,57 @@ class DESPermutation:
         # returns 48-bit
 
         E_TABLE = [
-            32,  1,  2,  3,  4,  5,
-            4,  5,  6,  7,  8,  9,
-            8,  9, 10, 11, 12, 13,
-            12, 13, 14, 15, 16, 17,
-            16, 17, 18, 19, 20, 21,
-            20, 21, 22, 23, 24, 25,
-            24, 25, 26, 27, 28, 29,
-            28, 29, 30, 31, 32,  1
+            32,
+            1,
+            2,
+            3,
+            4,
+            5,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            12,
+            13,
+            14,
+            15,
+            16,
+            17,
+            16,
+            17,
+            18,
+            19,
+            20,
+            21,
+            20,
+            21,
+            22,
+            23,
+            24,
+            25,
+            24,
+            25,
+            26,
+            27,
+            28,
+            29,
+            28,
+            29,
+            30,
+            31,
+            32,
+            1,
         ]
 
-        expanded_block = ''
+        expanded_block = ""
         for pos in E_TABLE:
             expanded_block += block_32bits[pos - 1]
 
@@ -71,22 +178,85 @@ class DESPermutation:
         # returns 64-bit (ciphertext block)
 
         inverse_initial_permut_table = [
-        40, 8, 48, 16, 56, 24, 64, 32,
-        39, 7, 47, 15, 55, 23, 63, 31,
-        38, 6, 46, 14, 54, 22, 62, 30,
-        37, 5, 45, 13, 53, 21, 61, 29,
-        36, 4, 44, 12, 52, 20, 60, 28,
-        35, 3, 43, 11, 51, 19, 59, 27,
-        34, 2, 42, 10, 50, 18, 58, 26,
-        33, 1, 41, 9,  49, 17, 57, 25
+            40,
+            8,
+            48,
+            16,
+            56,
+            24,
+            64,
+            32,
+            39,
+            7,
+            47,
+            15,
+            55,
+            23,
+            63,
+            31,
+            38,
+            6,
+            46,
+            14,
+            54,
+            22,
+            62,
+            30,
+            37,
+            5,
+            45,
+            13,
+            53,
+            21,
+            61,
+            29,
+            36,
+            4,
+            44,
+            12,
+            52,
+            20,
+            60,
+            28,
+            35,
+            3,
+            43,
+            11,
+            51,
+            19,
+            59,
+            27,
+            34,
+            2,
+            42,
+            10,
+            50,
+            18,
+            58,
+            26,
+            33,
+            1,
+            41,
+            9,
+            49,
+            17,
+            57,
+            25,
         ]
-    
+
         block = block_64bits
-    
+
         # Perform inverse permutation
-        plaintext = ''
+        plaintext = ""
         for pos in inverse_initial_permut_table:
-            plaintext += block[pos-1]
+            plaintext += block[pos - 1]
 
         # Return plaintext
         return plaintext
+
+if __name__ == "__main__":
+    permutation = DESPermutation()
+    binary_data = "0100100001100101011011000110110001101111001000010010000100100001"
+    print("Binary data:", binary_data)
+    blocks7bits = permutation.permuted_choice_1(binary_data)
+    print(blocks7bits)
