127c237c646134f7f96d203674d313393ab141a1
Jonathan-Rod <60677254+Jonathan-Rod@users.noreply.github.com>
2025-11-11 15:02:05 -0400

    feat: implement DESBitConverter for ASCII to binary conversion and vice versa

--- src/des_bit_converter.py
+++ src/des_bit_converter.py
@@ -1,6 +1,38 @@
 class DESBitConverter:
-    # TODO init constructor to specify block size
-    # TODO add function to convert string characters to binary string
-    # TODO add function to convert binary string to string
-    pass
 
+    def str_to_binary(self, string):  # TODO
+        """Convert a ascii string to binary string
+
+        Args:
+            string (str): The ascii string to convert
+
+        Returns:
+            str: The converted binary string
+        """
+        return "".join([format(ord(char), "08b") for char in string])
+
+    def binary_to_str(self, binary_string):  # TODO
+        """Convert a binary string to ascii string
+
+        Args:
+            binary_string (str): The binary string to convert
+
+        Returns:
+            str: The converted ascii string
+        """
+
+        return "".join(
+            [
+                chr(int(binary_string[i * 8 : i * 8 + 8], 2))
+                for i in range(len(binary_string) // 8)
+            ]
+        )
+
+
+if __name__ == "__main__":
+    converter = DESBitConverter()
+    test_string = "Hello"
+    binary = converter.str_to_binary(test_string)
+    print(f"String to binary: {binary}")
+    converted_back = converter.binary_to_str(binary)
+    print(f"Binary to string: {converted_back}")
